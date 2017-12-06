9d488dfdccc5eabed546663958bd7760c1580bb7
sumarouno <sumarsono.wongbandar@gmail.com>
2017-12-06 11:01:07 +0700

    mulai setup

--- jam_alarm_arduino_rtc.ino
+++ jam_alarm_arduino_rtc.ino
@@ -21,9 +21,36 @@ int hari;
 int menu = 0;
 int setAll = 0;
 
+uint8_t alarmHours = 0, alarmMinutes = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
+  lcd.init();
+  lcd.createChar(0, customChar);
+  lcd.backlight();
+  lcd.clear();
+
+  pinMode(P1, INPUT_PULLUP); // https://www.arduino.cc/en/Tutorial/InputPullupSerial
+  pinMode(P2, INPUT_PULLUP);
+  pinMode(P3, INPUT_PULLUP);
+  pinMode(P4, INPUT_PULLUP);
+  pinMode(output_1, OUTPUT);
+  pinMode(output_2, OUTPUT); 
+  //
+  Serial.begin(9600);
+  Wire.begin();
+  RTC.begin();
+
+  if (! RTC.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // Set the date and time at compile time
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+  // RTC.adjust(DateTime(__DATE__, __TIME__)); //removing "//" to adjust the time
+  // The default display shows the date and time
+  
+  int menu = 0;
 
 }
 
